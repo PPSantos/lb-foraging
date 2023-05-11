049b281e07af2b326a0bce0e41f81bfdee23d66f
PPSantos <pedro.pinto.santos@tecnico.ulisboa.pt>
2023-05-11 23:15:35 +0100

    Add normalization.

--- lbforaging/foraging/environment.py
+++ lbforaging/foraging/environment.py
@@ -83,6 +83,7 @@ class ForagingEnv(Env):
         max_episode_steps,
         force_coop,
         normalize_reward=True,
+        normalize_observation=True,
         grid_observation=False,
         penalty=0.0,
     ):
@@ -107,6 +108,7 @@ class ForagingEnv(Env):
 
         self._normalize_reward = normalize_reward
         self._grid_observation = grid_observation
+        self._normalize_observation = normalize_observation
 
         self.action_space = gym.spaces.Tuple(tuple([gym.spaces.Discrete(6)] * len(self.players)))
         self.observation_space = gym.spaces.Tuple(tuple([self._get_observation_space()] * len(self.players)))
@@ -394,25 +396,41 @@ class ForagingEnv(Env):
             ]
             # print("seen_players", seen_players)
 
+            field_y = self.field.shape[0]
+            field_x = self.field.shape[1]
+            obs_field_y = observation.field.shape[0]
+            obs_field_x = observation.field.shape[1]
+            max_food_level = self.max_player_level * len(self.players)
+
             for i in range(self.max_food):
                 obs[3 * i] = -1
                 obs[3 * i + 1] = -1
-                obs[3 * i + 2] = 0
+                obs[3 * i + 2] = -1
 
             for i, (y, x) in enumerate(zip(*np.nonzero(observation.field))):
                 obs[3 * i] = y
                 obs[3 * i + 1] = x
                 obs[3 * i + 2] = observation.field[y, x]
 
+                if self._normalize_observation:
+                    obs[3 * i] /= (obs_field_y - 1)
+                    obs[3 * i + 1] /= (obs_field_x - 1)
+                    obs[3 * i + 2] /= max_food_level
+
             for i in range(len(self.players)):
                 obs[self.max_food * 3 + 3 * i] = -1
                 obs[self.max_food * 3 + 3 * i + 1] = -1
-                obs[self.max_food * 3 + 3 * i + 2] = 0
+                obs[self.max_food * 3 + 3 * i + 2] = -1
 
             for i, p in enumerate(seen_players):
                 obs[self.max_food * 3 + 3 * i] = p.position[0]
                 obs[self.max_food * 3 + 3 * i + 1] = p.position[1]
                 obs[self.max_food * 3 + 3 * i + 2] = p.level
+
+                if self._normalize_observation:
+                    obs[self.max_food * 3 + 3 * i] /= (field_y - 1)
+                    obs[self.max_food * 3 + 3 * i + 1] /= (field_x - 1)
+                    obs[self.max_food * 3 + 3 * i + 2] /= self.max_player_level
 
             return obs
 
